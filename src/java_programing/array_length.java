8aa375e7659215968f29b86c3735780a6d8b0a42
이정호 <skjongho@naver.com>
2022-04-20 09:58:35 +0900

    배열

---
+++ src/java_programing/array_length.java
@@ -0,0 +1,30 @@
+package java_programing;
+
+import java.util.Scanner;
+
+public class array_length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("5개의 정수를 입력하세요.");
+		int intArray[]=new int[5];
+		
+		double sum=0.0;
+		for(int i=0; i<intArray.length;i++)
+		{
+			intArray[i]=scanner.nextInt();
+		}
+		
+		for(int i=0;i<intArray.length;i++)
+		{
+			sum+=intArray[i];
+		}
+		System.out.print("평균은 "+sum/intArray.length);
+		
+		scanner.close();
+
+	}
+
+}
