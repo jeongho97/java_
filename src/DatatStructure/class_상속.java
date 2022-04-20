562317006517042ddedc6a67026a2e5c7b207345
이정호 <skjongho@naver.com>
2022-04-20 16:55:36 +0900

    클래스 상속

---
+++ src/DatatStructure/class_상속.java
@@ -0,0 +1,40 @@
+package DatatStructure;
+
+class Point{
+	private int x,y;
+	void set(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	void showPoint() {
+		System.out.println("("+x+","+y+")");
+	}
+}
+
+class ColorPoint extends Point{
+	private String color;
+	void setColor(String color) {
+		this.color=color;
+	}
+	void showColorPoint() {
+		System.out.print(color);
+		showPoint();
+	}
+}
+public class class_»ó¼Ó {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p=new Point();
+		p.set(1,2);
+		p.showPoint();
+		
+		ColorPoint cp=new ColorPoint();
+		
+		cp.set(3, 4);
+		cp.setColor("red");
+		cp.showColorPoint();
+
+	}
+
+}
